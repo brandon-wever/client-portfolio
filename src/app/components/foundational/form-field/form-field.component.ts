c2c51488f8ce141dc755c8cf9e1406eeed8e34e1
Court Reeves <courtreeves6@gmail.com>
2021-08-04 08:50:12 -0400

    Adding basic form field design

--- src/app/components/foundational/form-field/form-field.component.ts
+++ src/app/components/foundational/form-field/form-field.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-form-field',
+  selector: 'fc-form-field',
   templateUrl: './form-field.component.html',
   styleUrls: ['./form-field.component.scss']
 })
 export class FormFieldComponent implements OnInit {
-
+  // https://material.angular.io/components/form-field/examples
   constructor() { }
 
   ngOnInit(): void {
