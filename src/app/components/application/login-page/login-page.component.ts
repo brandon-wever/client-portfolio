a6e12cd44126be3517bf260dbb63013246ed779f
Court Reeves <courtreeves6@gmail.com>
2021-08-04 18:14:07 -0400

    Adding a login form looks beautiful

--- src/app/components/application/login-page/login-page.component.ts
+++ src/app/components/application/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,14 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
+  hide: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get email() { return this.loginForm.get('email'); }
+
+  get password() { return this.loginForm.get('password'); }
+
   onFormSubmit(){
     console.log('submit button was clicked');
+    console.log(this.loginForm.value);
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    // MAKE LOGIN REQUEST
+    console.log('VALID')
   }
 
   onFormCancel(){
--- src/app/components/application/login-page/login-page.module.ts
+++ src/app/components/application/login-page/login-page.module.ts
@@ -3,8 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page.component';
 import { ButtonModule } from '../../foundational/button/button.module';
 import { FormFieldModule } from '../../foundational/form-field/form-field.module';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,13 @@ import { FormFieldModule } from '../../foundational/form-field/form-field.module
   imports: [
     CommonModule,
     ButtonModule,
-    FormFieldModule
+    FormFieldModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class LoginPageModule { }
